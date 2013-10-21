3635ed9c03d15cfbef7a0ef97f3ffe9aab8f7063
Juan Jose Garcia Ripoll <jjgarcia@users.sourceforge.net>
2013-10-21 15:43:21 +0200

    Move function types from f2c.h to f2c_types.h.in

--- include/f2c.h
+++ include/f2c.h
@@ -140,40 +140,6 @@ typedef struct Namelist Namelist;
 #define bit_clear(a,b)	((a) & ~((uinteger)1 << (b)))
 #define bit_set(a,b)	((a) |  ((uinteger)1 << (b)))
 
-/* procedure parameter types for -A and -C++ */
-
-#define F2C_proc_par_types 1
-#ifdef __cplusplus
-typedef int /* Unknown procedure type */ (*U_fp)(...);
-typedef shortint (*J_fp)(...);
-typedef integer (*I_fp)(...);
-typedef real (*R_fp)(...);
-typedef doublereal (*D_fp)(...), (*E_fp)(...);
-typedef /* Complex */ void (*C_fp)(...);
-typedef /* Double Complex */ void (*Z_fp)(...);
-typedef logical (*L_fp)(...);
-typedef shortlogical (*K_fp)(...);
-typedef /* Character */ void (*H_fp)(...);
-typedef /* Subroutine */ int (*S_fp)(...);
-#else
-typedef int /* Unknown procedure type */ (*U_fp)();
-typedef shortint (*J_fp)();
-typedef integer (*I_fp)();
-typedef real (*R_fp)();
-typedef doublereal (*D_fp)(), (*E_fp)();
-typedef /* Complex */ void (*C_fp)();
-typedef /* Double Complex */ void (*Z_fp)();
-typedef logical (*L_fp)();
-typedef shortlogical (*K_fp)();
-typedef /* Character */ void (*H_fp)();
-typedef /* Subroutine */ int (*S_fp)();
-#endif
-/* E_fp is for real functions when -R is not specified */
-typedef void C_f;	/* complex function */
-typedef void H_f;	/* character function */
-typedef void Z_f;	/* double complex function */
-typedef doublereal E_f;	/* real function with -R not specified */
-
 /* undef any lower-case symbols that your C compiler predefines, e.g.: */
 
 #ifndef Skip_f2c_Undefs
