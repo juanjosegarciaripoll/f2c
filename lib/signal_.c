a03cfcb33f8e199d567426c16dd972679aa8b56c
Juanjo Garcia-Ripoll <jjgarcia@users.sourcerforge.net>
2014-01-27 09:56:20 +0100

    Mark a problem in signal_()

--- lib/signal_.c
+++ lib/signal_.c
@@ -7,5 +7,9 @@ ftnint signal_(integer *sigp, void *proc)
 	int sig;
 	sig = (int)*sigp;
 
+	/* It is specified by GNU Fortran compiler that
+	 * SIGNAL returns the output of signal(2), even
+	 * if this is contradictory to the specification of
+	 * signal(), which does not return an integer. */
 	return (ftnint)signal(sig, (sig_pf)proc);
 }
