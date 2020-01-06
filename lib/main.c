8fc36065da18dfc2a4c5e873e62ede0cd18ec425
Carlo Dapor <catull@gmail.com>
2020-01-06 22:30:43 +0100

    Only use function attribute "weak" for non-macOS systems.

--- lib/main.c
+++ lib/main.c
@@ -23,7 +23,7 @@
 # endif
 #endif
 
-#ifdef HAVE_FUNC_ATTRIBUTE_WEAK
+#if defined(HAVE_FUNC_ATTRIBUTE_WEAK) && !defined(__APPLE__)
 /* Create dummy MAIN__ function and make MAIN__ a weak alias to it.
  * If linked against a program that exports MAIN__, i.e. one compiled
  * by f2c, this MAIN__ will resolve to the program's MAIN__ instead,
@@ -80,11 +80,10 @@ sig_die("Trace trap", 1);
 }
 #endif
 
-
 int xargc;
 char **xargv;
 
-#ifdef HAVE_FUNC_ATTRIBUTE_WEAK
+#if defined(HAVE_FUNC_ATTRIBUTE_WEAK) && !defined(__APPLE__)
 /* Make main() a weak symbol. */
 int main(int argc, char **argv) __attribute__((weak));
 int main(int argc, char **argv)
@@ -92,9 +91,9 @@ int main(int argc, char **argv)
   libf2c_init(argc, argv);
   MAIN_();
   libf2c_exit();
-  exit(0);	/* exit(0) rather than return(0) to bypass Cray bug */
-  return 0;	/* For compilers that complain of missing return values; */
-		/* others will complain that this is unreachable code. */
+  exit(0); /* exit(0) rather than return(0) to bypass Cray bug */
+  return 0;    /* For compilers that complain of missing return values; */
+       /* others will complain that this is unreachable code. */
 }
 #endif
 
@@ -102,7 +101,7 @@ void libf2c_init(int argc, char **argv)
 {
 xargc = argc;
 xargv = argv;
-signal1(SIGFPE, sigfdie);	/* ignore underflow, enable overflow */
+signal1(SIGFPE, sigfdie);  /* ignore underflow, enable overflow */
 #ifdef SIGIOT
 signal1(SIGIOT, sigidie);
 #endif
@@ -110,15 +109,15 @@ signal1(SIGIOT, sigidie);
 signal1(SIGTRAP, sigtrdie);
 #endif
 #ifdef SIGQUIT
-if(signal1(SIGQUIT,sigqdie) == SIG_IGN)
-	signal1(SIGQUIT, SIG_IGN);
+if (signal1(SIGQUIT, sigqdie) == SIG_IGN)
+   signal1(SIGQUIT, SIG_IGN);
 #endif
-if(signal1(SIGINT, sigindie) == SIG_IGN)
-	signal1(SIGINT, SIG_IGN);
-signal1(SIGTERM,sigtdie);
+if (signal1(SIGINT, sigindie) == SIG_IGN)
+   signal1(SIGINT, SIG_IGN);
+signal1(SIGTERM, sigtdie);
 
 #ifdef pdp11
-	ldfps(01200); /* detect overflow as an exception */
+   ldfps(01200); /* detect overflow as an exception */
 #endif
 
 f_init();
